8debcd943f0b2fec9ca98a30b70ef0b9b134fda2
lakimishvili <Liliana.Akimishvili@tbcacademy.edu.ge>
2026-01-11 12:22:45 +0400

    Initial Commit

---
+++ HealthPal/HealthPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthPalApp.swift
+//  HealthPal
+//
+//  Created by LILIANA on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
